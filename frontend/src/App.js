afd877b6041d94b30625b6cc4e82f8d62757d83b
Dopezapha <officialnwaneridaniel@gmail.com>
2024-08-18 00:47:58 +1000

    Added App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Web3Provider } from './contexts/Web3Context';
+import AccountInfo from './components/AccountInfo/AccountInfo';
+import StakingForm from './components/StakingForm/StakingForm';
+import YieldDisplay from './components/YieldDisplay/YieldDisplay';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3Provider>
+      <div className={styles.app}>
+        <h1>Bitcoin Staking and Yield Optimization</h1>
+        <AccountInfo />
+        <StakingForm />
+        <YieldDisplay />
+      </div>
+    </Web3Provider>
   );
 }
 
